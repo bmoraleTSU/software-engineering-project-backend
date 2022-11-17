51ea68ac892886626e922e570b25f0a1fb85a6c4
bmoraleTSU <bmorale1@my.tnstate.edu>
2022-11-17 11:59:07 -0600

    Adjusted SECRET_KEY to be like other secrets.

--- swe-project-backend-api/sweClassProjectBackend/sweClassProjectBackend/settings.py
+++ swe-project-backend-api/sweClassProjectBackend/sweClassProjectBackend/settings.py
@@ -20,6 +20,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
 SQL_USERNAME = os.environ.get('SQL_USERNAME')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
